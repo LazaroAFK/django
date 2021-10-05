ffa32083d4c575ad839b25c95d6976f98e396368
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-10-05 14:16:15 -0600

    Subiendo archivos

--- primera_app/wsgi.py
+++ primera_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for analisisdatos project.
+WSGI config for primera_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'analisisdatos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primera_app.settings')
 
 application = get_wsgi_application()
